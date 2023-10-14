5ad8993dcc380bb740d0a1838c2a0fb0b8cbe7d1
Komolika Kakrani <kanishkakakrani05@gmail.com>
2023-10-14 10:01:03 +0530

    program of a working calulator performing all the operations

---
+++ calculator.py
@@ -0,0 +1,33 @@
+#Write a program of a working calulator performing all the calculations
+
+import os
+
+while True: 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    x = input("Enter First Number : ")
+    operator = input("Choose an operator (+, -, *, /, %) ")
+    y = input("Enter Second Number : ")
+
+    if operator == "+":
+        print ("Your answer is ", int(x) + int(y))
+
+    elif operator == "-":
+        print ("Your answer is ", int(x) - int(y))
+
+    elif operator == "*":
+        print ("Your Answer is ", int(x) * int(y))
+
+    elif operator == "/":
+        print ("Your Answer is ", int(x) / int(y))
+
+    elif operator == "%":
+        print ("Your answer is ", int(x) % int(y))
+
+    else:
+        print ("Invalid Operation")
+
+    again = input("Do you want to use calculate again? Yes/No ")
+
+    if again == "no" :
+        break
